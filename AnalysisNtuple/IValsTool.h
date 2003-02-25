e128441a3068bd3ceb1a99dadd990b755a602b41
lsrea <>
2003-02-25 02:37:01 +0000

    rearrange inheritance to allow more common code complete set of tools prepare for tag v0r2

--- AnalysisNtuple/IValsTool.h
+++ AnalysisNtuple/IValsTool.h
@@ -8,42 +8,42 @@
 #include "GaudiKernel/IIncidentListener.h"
 
 // Declaration of the interface ID ( interface id, major version, minor version) 
-static const InterfaceID IID_IValsTool("IValsTool", 2 , 1); 
+static const InterfaceID IID_IValsTool("IValsTool", 2 , 2); 
 
 class INTupleWriterSvc;
 /** @class ITkrValsTool
 * @brief Abstract interface for tracker ntuple values 
 *
 * @author Leon Rochester
-* $Header: /nfs/slac/g/glast/ground/cvs/AnalysisNtuple/AnalysisNtuple/IValsTool.h,v 1.1.1.1 2003/02/23 21:31:10 burnett Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/AnalysisNtuple/AnalysisNtuple/IValsTool.h,v 1.2 2003/02/24 17:55:45 lsrea Exp $
 *
 */
 
 class   IValsTool : virtual public IAlgTool
 {
 public:
- 
+    
     /// Retrieve interface ID
     static const InterfaceID& interfaceID() { return IID_IValsTool; }
-
+    
     /// fill ntuple values into tupleSvc
     virtual StatusCode fillNtuple(INTupleWriterSvc* pSvc, std::string tupleName)=0;
     /// put one value into the ntuple, for rolling your own ntuple!
-    virtual StatusCode addNtupleValue(std::string valName, INTupleWriterSvc* pSvc,
-        std::string tupleName)=0;
+    virtual StatusCode fillNtuple(INTupleWriterSvc* pSvc,
+        std::string tupleName, std::string varName)=0;
     /// get a particular value, using ntuple name
     virtual StatusCode getVal(std::string varName, double& value)=0;
     /// output the list of names
-    virtual void announceBadName(std::string varName)=0;
+    //virtual void announceBadName(std::string varName)=0;
     /// output the names and values, either all (default) or just one;
     virtual void browseValues(std::string varName = "")=0;
-
+    
     /// calculate all values, over-ridden by XxxValsTool
     virtual StatusCode calculate()=0;
-
+    
     /// handle from incident service
     virtual void handle(const Incident& inc) = 0;
-
+    
 };
 
 #endif  // _H_ITkrValsTool
--- src/Dll/AnalysisNtuple_load.cxx
+++ src/Dll/AnalysisNtuple_load.cxx
@@ -3,14 +3,19 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/TkrUtil/src/Dll/TkrUtil_load.cxx,v 1.7 2003/02/17 18:06:58 lsrea Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/AnalysisNtuple/src/Dll/AnalysisNtuple_load.cxx,v 1.1.1.1 2003/02/23 21:31:10 burnett Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(AnalysisNtuple) {
-    DECLARE_TOOL(    TkrValsTool         );
-    DECLARE_TOOL(    CalValsTool         );
+    DECLARE_TOOL(    TkrValsTool      );
+    DECLARE_TOOL(    CalValsTool      );
+    DECLARE_TOOL(    AcdValsTool      );
+    DECLARE_TOOL(    MCValsTool       );
+    DECLARE_TOOL(    GltValsTool      );
+    DECLARE_TOOL(    TkrHitValsTool   );
+
 
 } 
 
