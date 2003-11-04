3a2039e6a86e698c97401bd4276a090e9d88a4a6
atwood <>
2003-11-04 21:51:20 +0000

    Widened range for EvtLogESum to 10 - 10^6 MeV.  Left in place  present limits (20 - 50000) on local use in log-E function corrections.

--- src/EvtValsTool.cxx
+++ src/EvtValsTool.cxx
@@ -3,7 +3,7 @@
 @brief Calculates the "Event" analysis variables from the other ntuple variables
 @author Bill Atwood, Leon Rochester
 
-$Header: /nfs/slac/g/glast/ground/cvs/AnalysisNtuple/src/EvtValsTool.cxx,v 1.8 2003/09/08 20:14:32 atwood Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/AnalysisNtuple/src/EvtValsTool.cxx,v 1.9 2003/10/12 07:06:43 lsrea Exp $
 */
 
 #include "ValBase.h"
@@ -282,10 +282,9 @@ StatusCode EvtValsTool::calculate()
             EvtCalEdgeAngle = (30. -calEdge)/sTkr;
         }
     }
-
     EvtPSFModel = sqrt(pow((.061/pow((std::max(EvtEnergySumOpt,1.)/100),.8)),2) + (.001745*.001745));
-    EvtLogESum = log10(std::min(std::max(EvtEnergySumOpt,20.),50000.));
-	double logE = EvtLogESum;
+    EvtLogESum = log10(std::min(std::max(EvtEnergySumOpt,10.),1000000.));
+	double logE = std::min(std::max(EvtLogESum,1.3), 4.7);
     double logE2 = logE*logE; 
     
 	double tkr1CovDet;
