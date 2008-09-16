e8632eef0e01c70e5e7535eb435e7a9034918236
ecephas <>
2008-09-16 19:34:31 +0000

    Replaced
          env.Tool('addLibrary', library = ['dl'])
          env.Tool('addLibrary', library = ['pthread'])
    
    With
          env.Tool('TkrUtilLib')
          env.Tool('ntupleWriterSvcLib')
          env.Tool('OnboardFilterLib')
          env.Tool('GlastSvcLib')
          env.Tool('FluxSvcLib')
          env.Tool('G4PropagatorLib')

--- AnalysisNtupleLib.py
+++ AnalysisNtupleLib.py
@@ -1,9 +1,12 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AnalysisNtuple/AnalysisNtupleLib.py,v 1.1 2008/08/15 21:42:33 ecephas Exp $
 def generate(env, **kw):
      if not kw.get('depsOnly', 0):
           env.Tool('addLibrary', library = ['AnalysisNtuple'])
-     env.Tool('addLibrary', library = ['dl'])
-     env.Tool('addLibrary', library = ['pthread'])
-
+     env.Tool('TkrUtilLib')
+     env.Tool('ntupleWriterSvcLib')
+     env.Tool('OnboardFilterLib')
+     env.Tool('GlastSvcLib')
+     env.Tool('FluxSvcLib')
+     env.Tool('G4PropagatorLib')
 def exists(env):
     return 1;
