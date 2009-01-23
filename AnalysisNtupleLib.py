24cdffc0a797c833a49e957ec0597265ee0669dc
cvs2git <>
2009-01-23 00:06:46 +0000

    This commit was manufactured by cvs2svn to create tag 'GlastRelease-15-49-00'.
    Sprout from master 2008-10-01 07:02:19 UTC lsrea 'AutoUpdate'
    Cherrypick from master 2009-01-23 00:06:44 UTC ecephas 'Check in for GlastRelease-15-49-00':
        AnalysisNtupleLib.py
        SConscript

--- AnalysisNtupleLib.py
+++ AnalysisNtupleLib.py
@@ -1,12 +1,18 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AnalysisNtuple/AnalysisNtupleLib.py,v 1.1 2008/08/15 21:42:33 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AnalysisNtuple/AnalysisNtupleLib.py,v 1.2 2008/09/16 19:34:30 ecephas Exp $
 def generate(env, **kw):
      if not kw.get('depsOnly', 0):
           env.Tool('addLibrary', library = ['AnalysisNtuple'])
+     env.Tool('EventLib')
      env.Tool('TkrUtilLib')
      env.Tool('ntupleWriterSvcLib')
      env.Tool('OnboardFilterLib')
      env.Tool('GlastSvcLib')
      env.Tool('FluxSvcLib')
      env.Tool('G4PropagatorLib')
+     env.Tool('addLibrary', library=env['obfLibs'])
+     env.Tool('RootIoLib')
+     env.Tool('facilitiesLib')
+     env.Tool('GlastSvcLib')
+     env.Tool('AcdUtilLib')
 def exists(env):
     return 1;
